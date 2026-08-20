7b5c28677bc3d1cbd2860b08ed99d9243ab155ba
alexfelipe182-spec <alexfelipe182@gmail.com>
2026-08-20 14:28:41 -0300

    feat: adicionar schemas pydantic

---
+++ backend/app/schemas/__init__.py
@@ -0,0 +1,7 @@
+from app.schemas.category import CategoryCreate, CategoryRead, CategoryUpdate
+from app.schemas.customer import CustomerCreate, CustomerRead, CustomerUpdate
+from app.schemas.product import ProductCreate, ProductRead, ProductUpdate
+from app.schemas.quote import QuoteCreate, QuoteRead, QuoteUpdate
+from app.schemas.user import UserCreate, UserRead, UserUpdate
+
+__all__ = ["CategoryCreate", "CategoryRead", "CategoryUpdate", "CustomerCreate", "CustomerRead", "CustomerUpdate", "ProductCreate", "ProductRead", "ProductUpdate", "QuoteCreate", "QuoteRead", "QuoteUpdate", "UserCreate", "UserRead", "UserUpdate"]
