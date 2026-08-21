6c32d2504ccfc20ccfc892e7b36e82f2e46808a4
alexfelipe182-spec <alexfelipe182@gmail.com>
2026-08-21 18:52:29 -0300

    feat: export password reset schemas

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -1,8 +1,10 @@
+from app.schemas.activity import ActivityRead
 from app.schemas.category import CategoryCreate, CategoryRead, CategoryUpdate
 from app.schemas.customer import CustomerCreate, CustomerRead, CustomerUpdate
+from app.schemas.password_reset import PasswordResetConfirm, PasswordResetRequest, PasswordResetResponse
 from app.schemas.product import ProductCreate, ProductRead, ProductUpdate
 from app.schemas.project import ProjectCreate, ProjectRead, ProjectUpdate
 from app.schemas.quote import QuoteCreate, QuoteRead, QuoteUpdate
 from app.schemas.user import UserCreate, UserRead, UserUpdate
 
-__all__ = ["CategoryCreate", "CategoryRead", "CategoryUpdate", "CustomerCreate", "CustomerRead", "CustomerUpdate", "ProductCreate", "ProductRead", "ProductUpdate", "ProjectCreate", "ProjectRead", "ProjectUpdate", "QuoteCreate", "QuoteRead", "QuoteUpdate", "UserCreate", "UserRead", "UserUpdate"]
+__all__ = ["ActivityRead", "CategoryCreate", "CategoryRead", "CategoryUpdate", "CustomerCreate", "CustomerRead", "CustomerUpdate", "PasswordResetConfirm", "PasswordResetRequest", "PasswordResetResponse", "ProductCreate", "ProductRead", "ProductUpdate", "ProjectCreate", "ProjectRead", "ProjectUpdate", "QuoteCreate", "QuoteRead", "QuoteUpdate", "UserCreate", "UserRead", "UserUpdate"]
