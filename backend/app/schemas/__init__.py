2358097394fd4c154fbc94f8a65fa70b18f89f6e
alexfelipe182-spec <alexfelipe182@gmail.com>
2026-08-21 18:51:34 -0300

    feat: export project schemas

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -1,7 +1,8 @@
 from app.schemas.category import CategoryCreate, CategoryRead, CategoryUpdate
 from app.schemas.customer import CustomerCreate, CustomerRead, CustomerUpdate
 from app.schemas.product import ProductCreate, ProductRead, ProductUpdate
+from app.schemas.project import ProjectCreate, ProjectRead, ProjectUpdate
 from app.schemas.quote import QuoteCreate, QuoteRead, QuoteUpdate
 from app.schemas.user import UserCreate, UserRead, UserUpdate
 
-__all__ = ["CategoryCreate", "CategoryRead", "CategoryUpdate", "CustomerCreate", "CustomerRead", "CustomerUpdate", "ProductCreate", "ProductRead", "ProductUpdate", "QuoteCreate", "QuoteRead", "QuoteUpdate", "UserCreate", "UserRead", "UserUpdate"]
+__all__ = ["CategoryCreate", "CategoryRead", "CategoryUpdate", "CustomerCreate", "CustomerRead", "CustomerUpdate", "ProductCreate", "ProductRead", "ProductUpdate", "ProjectCreate", "ProjectRead", "ProjectUpdate", "QuoteCreate", "QuoteRead", "QuoteUpdate", "UserCreate", "UserRead", "UserUpdate"]
