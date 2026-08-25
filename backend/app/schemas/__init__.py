0ea22c13caf410335c087e96b44830c0f6aad624
alexfelipe182-spec <alexfelipe182@gmail.com>
2026-08-25 09:21:47 -0300

    refactor: export typed quote estimate schema

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -4,7 +4,15 @@ from app.schemas.customer import CustomerCreate, CustomerRead, CustomerUpdate
 from app.schemas.password_reset import PasswordResetConfirm, PasswordResetRequest, PasswordResetResponse
 from app.schemas.product import ProductCreate, ProductRead, ProductUpdate
 from app.schemas.project import ProjectCreate, ProjectRead, ProjectUpdate
-from app.schemas.quote import QuoteCreate, QuoteRead, QuoteUpdate
+from app.schemas.quote import QuoteCreate, QuoteEstimateResponse, QuoteRead, QuoteUpdate
 from app.schemas.user import UserCreate, UserRead, UserUpdate
 
-__all__ = ["ActivityRead", "CategoryCreate", "CategoryRead", "CategoryUpdate", "CustomerCreate", "CustomerRead", "CustomerUpdate", "PasswordResetConfirm", "PasswordResetRequest", "PasswordResetResponse", "ProductCreate", "ProductRead", "ProductUpdate", "ProjectCreate", "ProjectRead", "ProjectUpdate", "QuoteCreate", "QuoteRead", "QuoteUpdate", "UserCreate", "UserRead", "UserUpdate"]
+__all__ = [
+    "ActivityRead", "CategoryCreate", "CategoryRead", "CategoryUpdate",
+    "CustomerCreate", "CustomerRead", "CustomerUpdate",
+    "PasswordResetConfirm", "PasswordResetRequest", "PasswordResetResponse",
+    "ProductCreate", "ProductRead", "ProductUpdate",
+    "ProjectCreate", "ProjectRead", "ProjectUpdate",
+    "QuoteCreate", "QuoteEstimateResponse", "QuoteRead", "QuoteUpdate",
+    "UserCreate", "UserRead", "UserUpdate",
+]
