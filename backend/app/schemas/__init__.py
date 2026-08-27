a0f18e6ca33b7dd12910167bf5d0f3ce02751774
alexfelipe182-spec <alexfelipe182@gmail.com>
2026-08-27 12:01:01 -0300

    feat: export production cost schemas

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -3,6 +3,17 @@ from app.schemas.category import CategoryCreate, CategoryRead, CategoryUpdate
 from app.schemas.customer import CustomerCreate, CustomerRead, CustomerUpdate
 from app.schemas.password_reset import PasswordResetConfirm, PasswordResetRequest, PasswordResetResponse
 from app.schemas.product import ProductCreate, ProductRead, ProductUpdate
+from app.schemas.production_cost import (
+    MaterialCreate,
+    MaterialRead,
+    MaterialUpdate,
+    ProjectCostCreate,
+    ProjectCostRead,
+    ProjectCostUpdate,
+    SupplierCreate,
+    SupplierRead,
+    SupplierUpdate,
+)
 from app.schemas.project import ProjectCreate, ProjectRead, ProjectUpdate
 from app.schemas.quote import QuoteCreate, QuoteEstimateResponse, QuoteRead, QuoteUpdate
 from app.schemas.quote_item import QuoteItemCreate, QuoteItemRead, QuoteItemUpdate
@@ -11,10 +22,13 @@ from app.schemas.user import UserCreate, UserRead, UserUpdate
 __all__ = [
     "ActivityRead", "CategoryCreate", "CategoryRead", "CategoryUpdate",
     "CustomerCreate", "CustomerRead", "CustomerUpdate",
+    "MaterialCreate", "MaterialRead", "MaterialUpdate",
     "PasswordResetConfirm", "PasswordResetRequest", "PasswordResetResponse",
     "ProductCreate", "ProductRead", "ProductUpdate",
+    "ProjectCostCreate", "ProjectCostRead", "ProjectCostUpdate",
     "ProjectCreate", "ProjectRead", "ProjectUpdate",
     "QuoteCreate", "QuoteEstimateResponse", "QuoteRead", "QuoteUpdate",
     "QuoteItemCreate", "QuoteItemRead", "QuoteItemUpdate",
+    "SupplierCreate", "SupplierRead", "SupplierUpdate",
     "UserCreate", "UserRead", "UserUpdate",
 ]
